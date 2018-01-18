11ae5b8d9b8053ffed695b7d1a9011bf4e3a666b
iKichiemon <1990.iku.t@gmail.com>
2018-01-18 20:40:30 +0900

    update

--- RxFirebase/Classes/FirebaseAnalytics+Rx.swift
+++ RxFirebase/Classes/FirebaseAnalytics+Rx.swift
@@ -1,4 +1,51 @@
 import RxSwift
+import FirebaseStorage
+
+public extension Reactive where Base: StorageReference {
+
+    // MARK - put
+
+    public func putData(uploadData: Data, metadata: StorageMetadata? = nil) -> Observable<StorageUploadTask> {
+        return .create { observer in
+            observer.onNext(self.base.putData(uploadData, metadata: metadata, completion: { (metadata, error) in
+            }))
+            return Disposables.create()
+        }
+    }
+
+    public func putFile(from url: URL, metadata: StorageMetadata? = nil) -> Observable<StorageUploadTask> {
+        return .create { observer in
+            observer.onNext(self.base.putFile(from: url, metadata: metadata, completion: { (metadata, error) in
+            }))
+            return Disposables.create()
+        }
+    }
+
+
+    // MARK - get
+
+    public func getData(maxSize: Int64) -> Observable<Data> {
+        return .create { observer in
+            self.base.getData(maxSize: maxSize) { (data, error) in
+                observer.onNext()
+            }
+            return Disposables.create()
+        }
+    }
+
+    public func getMetadata() -> Observable<StorageDownloadTask> {
+        return .create { observer in
+            self.base.getMetadata(completion: { metadata, error in
+                switch (metadata, error) {
+                case (.some(let meta), .none): observer.onNext(meta)
+                case (.none, .some(let error)): observer.onError(error)
+                default: fatalError()
+                }
+            })
+            return Disposables.create()
+        }
+    }
+}
 
 public struct Test {
     let a: String
@@ -6,6 +53,7 @@ public struct Test {
         self.a = a
     }
     func b() {
+        let store = Storage.storage().reference().child("a")
     }
 }
 
